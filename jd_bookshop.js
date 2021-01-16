f5f81a907659f0eb4bed87227130205623cc7504
wishcaiji <3219904322@qq.com>
2021-01-16 21:20:05 +0800

    Update jd_bookshop.js

--- jd_bookshop.js
+++ jd_bookshop.js
@@ -1,6 +1,6 @@
 /*
 口袋书店
-活动入口：京东app首页-京东图书-右侧口袋书店
+活动入口：京东app首页-京东图书-右侧口袋书店-就可以了
 已支持IOS双京东账号,Node.js支持N个京东账号
 脚本兼容: QuantumultX, Surge, Loon, JSBox, Node.js
 ============Quantumultx===============
